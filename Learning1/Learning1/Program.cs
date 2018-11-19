57e3eb97d088627fb61ba9bd77004bf78cd475b4
xiaoyi <linyihuang@hitech.com>
2018-11-19 12:43:37 +0800

    Test GIT

--- Learning1/Learning1/Program.cs
+++ Learning1/Learning1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* TestHello
 namespace Learning1
 {
     class Program
@@ -11,7 +12,45 @@ namespace Learning1
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+
             Console.ReadKey();
+        }
+    }
+}
+*/
+ 
+
+
+namespace YichangLearning
+{
+    class TestYichang
+    {
+        public int result;
+        public TestYichang()
+        {
+            result = 0;
+        }
+
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestYichang app = new TestYichang();
+            app.division(25, 0);
+            Console.ReadLine();
         }
     }
 }
